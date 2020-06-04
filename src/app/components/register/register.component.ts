3abcf6a0f27874a1b7c08904ce4c750f715321f5
meevikash <mevikash@iitk.ac.in>
2020-06-04 20:44:40 +0530

    second commit

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class RegisterComponent {
+isAvailable = true;
+products = [
+  {name: 'Delhi', district : 'alpha'},
+  {name: 'Mumbai', district : 'beta' },
+  {name: 'Kolkata', district : 'gamma'},
+ ]
 }
